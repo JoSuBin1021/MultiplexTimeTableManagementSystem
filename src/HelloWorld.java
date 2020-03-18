d5f44d27b6efb3d265ff3c9e6097050d9bb6d2dd
reals <reals@DESKTOP-A24S3D8>
2020-03-18 22:57:21 +0900

    Create a Menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("** Multiplex TimeTable Management System Menu **");
+		System.out.println(" 1. Add Movie");
+		System.out.println(" 2. Delete Movie");
+		System.out.println(" 3. Edit Movie");
+		System.out.println(" 4. View Movie");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1-5");
+	}
+}
